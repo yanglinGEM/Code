faaa162f8c325dcdbddedaa9074ca413a9e39a89
yanglinGEM <68836138+yanglinGEM@users.noreply.github.com>
2020-12-12 20:46:39 +0800

    12.12

---
+++ Code/12.12.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return n;
+        }
+        vector<int> up(n), down(n);
+        up[0] = down[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            }
+            else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = max(up[i - 1] + 1, down[i - 1]);
+            }
+            else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return max(up[n - 1], down[n - 1]);
+    }
+};
+
+int main() {
+    vector<int> nums;
+    int n;
+    cin >> n;
+    while(n!=0){
+        nums.push_back(n);
+        cin >> n;
+    }
+    Solution s;
+    cout << s.wiggleMaxLength(nums) << endl;
+}
