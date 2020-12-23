35ab3a220047bc024b0ac02b4c4cc3ead710e544
yanglinGEM <68836138+yanglinGEM@users.noreply.github.com>
2020-12-23 11:08:58 +0800

    12.23

--- Code/12.23.cpp
+++ Code/12.23.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 class Solution {
 public:
+    //int firstUniqChar(string s) {
+    //    int n = s.size();
+    //    if (n == 1)return 0;
+    //    int i = 0, j = 0;
+    //    for(i=0;i<n;i++){
+    //        for (j = 0; j < n; j++) {
+    //            if (s[i] == s[j]&&i!=j) break;
+    //        }
+    //        if (j == n)return i;
+    //    }
+    //    return -1;
+    //}
     int firstUniqChar(string s) {
         int n = s.size();
-        if (n == 1)return 0;
-        int i = 0, j = 0;
-        for(i=0;i<n;i++){
-            for (j = 0; j < n; j++) {
-                if (s[i] == s[j]&&i!=j) break;
-            }
-            if (j == n)return i;
+        int arr[26] = { 0 };
+        for (int i = 0; i < n; i++) {
+            arr[s[i] - 'a']++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[s[i] - 'a' ] == 1)return i;
+
         }
         return -1;
     }
